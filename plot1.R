35c263bcd8442149e73d930f555bb771b6d364b8
darwinmommy <darwinmommy@gmail.com>
2018-07-06 16:16:04 -0700

    Update plot1.R
    Minor annotation update

--- plot1.R
+++ plot1.R
@@ -14,6 +14,7 @@ NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
 #Plot 1:
+#Use tapply to sum the PM2.5 values
 plot1<-with(NEI,tapply(Emissions,list(year),FUN = sum))
 plot(plot1,type="o",pch=19,ylab="Total PM2.5 Emissions (tons)",xlab="Year",main = "Total PM2.5 Emissions by Year",xaxt="n")
 axis(1,at=c(1,2,3,4),labels = c("1999","2002","2005","2008"))
