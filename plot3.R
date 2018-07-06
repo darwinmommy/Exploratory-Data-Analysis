35b932150d09affd1dd62e3e03e9478e69371351
darwinmommy <darwinmommy@gmail.com>
2018-07-06 15:15:33 -0700

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,32 @@
+#Download and unzip
+filename<-"PM Data.zip"
+if(!file.exists(filename)){
+    URL<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+    download.file(URL,filename)
+}
+
+if(!file.exists("PM Data")){
+    unzip(filename, exdir="PM Data")
+}
+
+#Read the data files
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+
+#Plot 3:
+plot3<-data.frame(with(Balt,tapply(Emissions,list(type,year), FUN = sum)))
+colnames(plot3)<-c("1999","2002","2005","2008")
+plot3$Type<-c("Non-Road","Non-Point","On-Road","Point")
+library(reshape2)
+plot3m<-melt(plot3,id.vars="Type",
+     variable.name="Year",
+     value.name = "TotalPM")
+library(ggplot2)
+ggplot(data=plot3m, aes(x=Year,y=TotalPM, col=Type,group=Type))+
+    geom_line()+
+    geom_point(size=3)+
+    labs(title="PM25 by Source Type in Baltimore City, MD",y="Total PM25 (tons)")+
+    theme(plot.title=element_text(hjust=0.5,size = 20))
+dev.copy(png,"plot3.png")
+dev.off()
