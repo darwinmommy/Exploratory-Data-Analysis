52ebd640d25187fa4da1a66cee80de7e1cfac112
darwinmommy <darwinmommy@gmail.com>
2018-07-06 16:32:50 -0700

    Update plot5.R
    Annotation updates

--- plot5.R
+++ plot5.R
@@ -15,13 +15,21 @@ SCC <- readRDS("Source_Classification_Code.rds")
 
 
 #Plot 5:
+#Change the SCC.Level.Two columns from factor to character
 SCC$SCC.Level.Two<-as.character(as.factor(SCC$SCC.Level.Two))
+#Subset including only rows that include the word "Motor" in the SCC.Level.Two column
 subSCC<-subset(SCC,grepl("Vehicles",SCC.Level.Two))
+#Create a vector of the SCC codes resulting from the subset above
 motorSCC<-as.vector(subSCC$SCC)
+#Subset the PM2.5 data to include only rows with the SCC codes in the vector above
 motorSub<-NEI[motorSCC %in% NEI$SCC,]
+#Subset the motor vehicle data from above to include only Baltimore City, MD
 motorBalt<-subset(motorSub,fips=="24510")
+#Use tapply to sum the PM2.5 emissions by year
 plot5<-with(motorBalt,tapply(Emissions,list(year), FUN = sum))
+#Begin writing the png file
 png("plot5.png")
 plot(plot5, type="o",pch=19,ylab="Total PM2.5 Emissions (tons)",xlab="Year",main = "Total PM2.5 Emissions for Baltimore City, MD \nMotor Vehicle Sources",xaxt="n")
 axis(1,at=c(1,2,3,4),labels = c("1999","2002","2005","2008"))
+#Turn off and save png file
 dev.off()
